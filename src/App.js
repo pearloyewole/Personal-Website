c791842e3ed474daf1ba28558c9507db8a0805b1
pearloyewole <pearloyewole1@gmail.com>
2025-01-10 16:43:33 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hi! I'm Pearl </h1>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default App;
