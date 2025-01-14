01c5169333dcc07e32f7010f1bcce096575cd523
pearloyewole <pearloyewole1@gmail.com>
2025-01-14 10:41:56 -0800

    Update App.js with personal introduction

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hi! I'm Pearl </h1>
+      <h1> Hi! I'm Pearl. </h1>
+      <h4> Freshman @ Caltech Studying Computer Science</h4>
+      <p>Hi! I am Pearl. An undergraduate Computer Science student at the California Institute of Technology (Caltech). I am interested in using technology to work on impactful, real-world problems! This website also serves as an example of my website development capabilities.</p>
       <button>Click me</button>
     </div>
   );
 }
 
-export default App;
+export default App
