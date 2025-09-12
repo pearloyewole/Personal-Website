7dc2de995c0e424d675150c9d62e13dbaa1da2b6
pearloyewole <pearloyewole1@gmail.com>
2025-09-12 13:37:07 -0500

    w

--- src/components/IndustryExperience.js
+++ src/components/IndustryExperience.js
@@ -29,7 +29,7 @@ function IndustryExperience() {
       title: "Data Visualization Tools Engineer",
       date: "March 2025 - Present",
       tools: ["D3.js", "Three.js", "React", "WebGL", "HCI", "Data Visualization"],
-      description: "Developing full-stack data visualization software to handle large 3D ADCP and satellite datasets. Implemented backend and computation pipelines to support dataset uploads, cross-section extraction, and fluid shear stress calculations, optimizing performance for interactive exploration. Collaborating with researchers to deliver a scientific tool that visualizes river systems in 3D, link hydrodynamic forces to erosion patterns, and generate new insights from Alaskan river datasets.",
+      description: "Developing full-stack data visualization software to handle large 3D ADCP and satellite datasets. Implementing backend and computation pipelines to support dataset uploads, cross-section extraction, and fluid shear stress calculations, optimizing performance for interactive exploration. Collaborating with researchers to deliver a scientific tool that visualizes river systems in 3D, link hydrodynamic forces to erosion patterns, and generate new insights from Alaskan river datasets.",
       location: "Pasadena, CA",
     },
     {
